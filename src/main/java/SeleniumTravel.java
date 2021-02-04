0e83aff3a0adb349e3f5d9656d94383b88baf174
Kenoid <konqer@gmail.com>
2021-02-04 23:12:15 +0100

    second commit

--- src/main/java/SeleniumTravel.java
+++ src/main/java/SeleniumTravel.java
@@ -2,6 +2,15 @@ public class SeleniumTravel {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        String kong = "Chuj ci na kurwe";
+        kong = kong.toLowerCase();
+
+        System.out.println(kong);
+
+
+
     }
 
 }
